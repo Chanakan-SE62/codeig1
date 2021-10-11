5ef96b37b46ae128cad65515c034f62f98767e4f
Chanakan <624259009@webmail.npru.ac.th>
2021-10-11 12:28:04 +0700

    RestaurantModel

--- app/Models/RestaurantModel.php
+++ app/Models/RestaurantModel.php
@@ -7,5 +7,5 @@
 class RestaurantModel extends Model{
     protected $table = 'restaurants'; //เข้าถึงตารางชื่อ restaurants
     protected $primaryKey = 'id'; //กำหนด primaryKey
-    protected $allowdFields = ['id', 'name', 'type', 'imageurl']; //อนุญาตให้เข้าถึงฟิวเหล่านี้ได้
+    protected $allowedFields = ['id', 'name', 'type', 'imageurl']; //อนุญาตให้เข้าถึงฟิวเหล่านี้ได้
 }
